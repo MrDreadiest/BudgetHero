d33b6538089353baeb21d168f542460986a2c144
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-11-18 14:13:08 +0100

    Add project files.

---
+++ BudgetHero.App/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace BudgetHero.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ BudgetHero.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetHero.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
