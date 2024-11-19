af061fdbb40887be6c6765981578da0265ad5404
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-11-19 16:30:36 +0100

    Podpięcie serwisów.

--- BudgetHero.App/MauiProgram.cs
+++ BudgetHero.App/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using BudgetHero.App.Services;
+using BudgetHero.App.Services.Interfaces;
+using BudgetHero.App.ViewModels;
+using CommunityToolkit.Maui;
+using Syncfusion.Maui.Toolkit.Hosting;
 
 namespace BudgetHero.App
 {
@@ -9,6 +13,11 @@ namespace BudgetHero.App
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureSyncfusionToolkit()
+                .ConfigureMauiHandlers(handlers =>
+                {
+                })
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -16,8 +25,34 @@ namespace BudgetHero.App
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
+
+            #region Services
+
+            builder.Services.AddSingleton<IModalDisplayHandler, ModalDisplayHandler>();
+            builder.Services.AddSingleton<IUserService, UserService>();
+            builder.Services.AddSingleton<IBudgetService, BudgetService>();
+            builder.Services.AddSingleton<ITransactionCategoryService, TransactionCategoryService>();
+            builder.Services.AddSingleton<ITransactionService, TransactionService>();
+
+            builder.Services.AddTransient<IAppSettingsService, AppSettingsService>();
+            builder.Services.AddTransient<IConfigurationService, ConfigurationService>();
+            builder.Services.AddTransient<IApiClient, ApiClient>();
+            builder.Services.AddTransient<IRegisterService, RegisterService>();
+            builder.Services.AddTransient<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddTransient<ITokenService, TokenService>();
+
+            #endregion
+
+            #region ViewModels
+
+            builder.Services.AddSingleton<MainViewModel>();
+
+            #endregion
+
+
+
 
             return builder.Build();
         }
