4f623e71945199f7008ab2839c8c0b6ddc0919e6
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-11-25 17:09:40 +0100

    Dodanie fabryki raportowania.

---
+++ BudgetHero.App/Factories/Reports/IReportFactory.cs
@@ -0,0 +1,9 @@
+﻿using BudgetHero.App.ViewModels.Content.Reports;
+
+namespace BudgetHero.App.Factories.Reports
+{
+    public interface IReportFactory
+    {
+        IReport CreateReport(ReportViewModel reportViewModel);
+    }
+}
---
+++ BudgetHero.App/Factories/Reports/ReportGraphBarFactory.cs
@@ -0,0 +1,12 @@
+﻿using BudgetHero.App.ViewModels.Content.Reports;
+
+namespace BudgetHero.App.Factories.Reports
+{
+    public class ReportGraphBarFactory : IReportFactory
+    {
+        public IReport CreateReport(ReportViewModel reportViewModel)
+        {
+            return new ReportBarViewModel(reportViewModel);
+        }
+    }
+}
