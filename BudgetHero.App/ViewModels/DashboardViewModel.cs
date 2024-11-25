9a6207483ed93992d52cb093957f3fc294a19744
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-11-25 20:37:08 +0100

    Wsyawienie okna kokpitu. Wyrónanie nagłówków okien głównych.

--- BudgetHero.App/ViewModels/DashboardViewModel.cs
+++ BudgetHero.App/ViewModels/DashboardViewModel.cs
@@ -1,23 +1,36 @@
-﻿using BudgetHero.App.Services.Interfaces;
+﻿using BudgetHero.App.Models;
+using BudgetHero.App.Services.Interfaces;
+using BudgetHero.App.Utilities;
 using BudgetHero.App.ViewModels.Interfaces;
+using BudgetHero.App.Views.Details;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace BudgetHero.App.ViewModels
 {
     public partial class DashboardViewModel : ViewModelBase, IBusyHandler
     {
+        [ObservableProperty]
+        private Budget _budget;
+
         public IModalDisplayHandler? ModalDisplayHandler => _displayHandler;
 
         private bool _isNavigatedTo;
         private bool _dataLoaded;
 
         private readonly IModalDisplayHandler _displayHandler;
+        private readonly IBudgetService _budgetService;
 
-        public DashboardViewModel(IModalDisplayHandler displayHandler)
+        public DashboardViewModel(IModalDisplayHandler displayHandler, IBudgetService budgetService)
         {
             _displayHandler = displayHandler;
+            _budgetService = budgetService;
+
             Title = Resources.Languages.AppResource.DashboardView_Title;
         }
+
+        [RelayCommand]
+        private void NavigatedToBudgetDetails() => Shell.Current.GoToAsync($"{nameof(BudgetDetailView)}?id={Budget.Id}").FireAndForgetSafeAsync(ModalDisplayHandler);
 
         [RelayCommand]
         private void NavigatedTo() => _isNavigatedTo = true;
@@ -43,7 +56,11 @@ namespace BudgetHero.App.ViewModels
 
         private async Task Refresh()
         {
-            await Task.CompletedTask;
+            await this.RunWithBusyFlagAsync(async () =>
+            {
+                Budget = _budgetService.CurrentBudget;
+                await Task.CompletedTask;
+            });
         }
 
         private async Task InitData()
