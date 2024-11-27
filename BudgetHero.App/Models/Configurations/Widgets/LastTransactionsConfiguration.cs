d788c6dcebedb1d23f76a649d14ac5c0697ef01f
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-11-27 21:46:58 +0100

    Dodanie konfiguracji widgetu ostatnich transakcji.

--- BudgetHero.App/Models/Configurations/Widgets/LastTransactionsConfiguration.cs
+++ BudgetHero.App/Models/Configurations/Widgets/LastTransactionsConfiguration.cs
@@ -4,5 +4,13 @@ namespace BudgetHero.App.Models.Configurations.Widgets
     [ResourceKey("Resource_Singular_Configuration", "Resource_Singular_Configurations")]
     public class LastTransactionsConfiguration
     {
+        private const int DefaultLastTransactionCount = 5;
+
+        public int Count { get; set; }
+
+        public LastTransactionsConfiguration()
+        {
+            Count = DefaultLastTransactionCount;
+        }
     }
 }
